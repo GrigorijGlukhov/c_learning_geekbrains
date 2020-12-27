0cb73c0f01799c4142f87d2c720ef7bd59bac9c8
Grigorij Glukhov <grigorij.gluhov@gmail.com>
2020-12-27 17:43:45 +0300

    - c lessons. Initial commit.

---
+++ main.c
@@ -0,0 +1,12 @@
+// Copyright (C) Grigorij Glukhov. 2020. All rigths reserved
+// For learning purposes
+ 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main( int argv, char* argc[] )
+{
+
+  return 0;
+}
