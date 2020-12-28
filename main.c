5339acd81c02085cee0795716b4e5e4d6b901b94
Grigory.Glukhov <grigorij.gluhov@gmail.com>
2020-12-29 00:39:31 +0300

    - lesson 04: loops while and do-while

--- main.c
+++ main.c
@@ -8,39 +8,22 @@
 
 int main( int argc, const char* argv[] )
 {
-    char answer;
-    printf("Do you want to salute you? (y/n)");
-    scanf("%c", &answer); // scanf uses pointer for char
-    if ( answer == 'y' ) {
-        printf("\nHello, user!\n");
-    } else if ( answer == 'n' ) {
-        printf("\nI didn't want to salute you anyway!\n");
-    } else {
-        printf("\nI can't understand your input!\n");
+    int a = 10;
+    int b = 20;
+    int even = 0;
+    while ( a < b ) {
+        if ( a % 2 == 0 )
+            even++;
+        a++;
     }
+    printf("There are %d even numbers in sequence\n", even);
 
-    int a = 10, b = 15;
-    a = ( a > b ) ? b : a;
-    if ( a > b )
-        a = b;
-    else
-        a = 0;
-
-    printf("\n");
-    printf("%s", (1>0) ? "true\n" : "false\n");
-
-    // > < >= <= == 
-    // && || ! ^
-    if ( 1 && 0 ) printf("false\n");
-    if ( 1 || 0 ) printf("true\n");
-    if ( !(1) ) printf("false\n");
-    if ( 1 ^ 1 ) printf("false\n");
-
-    printf("\n");
-    int x = 7;
-    if ( (x >= 0) && (x <= 10) ) {
-        printf("X Fits!\n");
-    }
-
+    int input;
+    do {
+        printf("Enter a divider for 100 (not zero)\n");
+        scanf("%d", &input);
+    } while ( input == 0 );
+    printf("100 / %d = %d\n", input, 100 / input);
+    printf("and the reminder will be %d, by the way\n",100 % input);
     return 0;
 }
